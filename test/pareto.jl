0530a58463bcfc2f2c8ea4c5f900af92310cdfe5
jpryan1 <johnpaulryan24@gmail.com>
2021-05-26 08:57:31 -0400

    pa re to fa sol la ti do

---
+++ test/pareto.jl
@@ -0,0 +1,56 @@
+# WARNING: new version deprecates this, future profiling with @profile
+
+using LinearAlgebra
+using FastKernelTransform
+using FastKernelTransform: gaussian_mixture_data, two_bump_data, uniform_data, unit_hypersphere, FactorizationParameters
+using TimerOutputs
+using StaticArrays
+using Profile
+using CovarianceFunctions
+using CovarianceFunctions: Exp, EQ, MaternP, Matern, Cauchy, difference
+using FastKernelTransform: FmmMatrix, factorize
+using NearestNeighbors
+
+ek(r) = exp(-r) # with short lengthscale, not as accurate?
+ek(x, y) = ek(norm(difference(x, y)))
+FastKernelTransform.qrable(::typeof(ek)) = false
+GC.gc()
+
+function run_pareto_test(b, x, scale, kernel, points, param, out)
+    params = FactorizationParameters(max_dofs_per_leaf = 128, precond_param = 0,
+                                    trunc_param = param, neighbor_scale=scale, lazy = true)
+    mat = FmmMatrix(kernel, points, params)
+
+    elapsed_time = @elapsed begin
+        fact = factorize(mat)
+        bbar = *(fact, x, verbose = true)
+    end
+    rel_err = norm(b-bbar)/norm(b)
+    if out println(elapsed_time,",",rel_err) end
+end
+
+function run_time_trial()
+
+    N = 10000  # Number of points
+    dimension = 2
+    to = TimerOutput()
+    alpha = dimension/2 - 1
+    data_generator(n, d) = uniform_data(n, d)
+    points = data_generator(N, dimension)
+    kernel = ek
+    x = rand(N)
+
+    kern_mat = zeros(length(points), length(points))
+    kern_mat .= kernel.(points, permutedims(points))
+    b = kern_mat * x
+    for param in 0:2:6
+        println("p=", param)
+        for scale in [0.3 0.4 0.5 0.6 0.7]
+    # bigger neighbor scale means more compression, less accuracy
+            run_pareto_test(b, x, scale, kernel, points, param, false)
+            run_pareto_test(b, x, scale, kernel, points, param, true)
+            GC.gc()
+        end
+    end
+end
+run_time_trial()
